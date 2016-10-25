3c0bb99b13284e9273ec8bdbd0c9ec672b38a20d
Robert Hustead <husteadrobert@gmail.com>
2016-10-25 16:53:40 +0900

    Adding Medium 1

---
+++ Exercises/130 Exercises/Medium 1.rb
@@ -0,0 +1,104 @@
+# Listening Device
+class Device
+  def initialize
+    @recordings = []
+  end
+
+  def listen
+    record(yield) if block_given?
+  end
+  def record(recording)
+    @recordings << recording
+  end
+
+  def play
+    puts "#{@recordings.last}"
+  end
+end
+
+# Text Analyzer - Sandwich Code
+class TextAnalyzer
+  def process
+    whole_text = ""
+    f = File.open("random.txt", "r")
+    f.each_line do |line|
+      whole_text << line
+    end
+    yield(whole_text)
+    f.close
+  end
+end
+
+analyzer = TextAnalyzer.new
+analyzer.process { |file| puts "#{whole_text.split("\n\n").count} paragraphs"}
+analyzer.process { |file| puts "#{whole_text.split("\n").count} lines"}
+analyzer.process { |file| puts "#{whole_text.split(' ').count} lines"}
+
+#LS Solution
+class TextAnalyzer
+  def process
+    file = File.open('sample_text.txt', 'r')
+    yield(file.read)
+    file.close
+  end
+end
+
+analyzer = TextAnalyzer.new
+analyzer.process { |text| puts "#{text.split("\n\n").count} paragraphs" }
+analyzer.process { |text| puts "#{text.split("\n").count} lines" }
+analyzer.process { |text| puts "#{text.split(' ').count} words" }
+
+# Passing Parameters Part 1
+def gather(items)
+  puts "Let's start gathering food."
+  yield(items)
+  puts "Lets start gathering food."
+end
+
+gather(items) {puts "#{items.join('; ')}"}
+
+# Passing Parameters Part 2
+#LS Solution
+birds = ['crow', 'finch', 'hawk', 'eagle']
+
+def types(birds)
+  yield birds
+end
+
+types(birds) do |_,_,*birds_of_prey|
+  puts "Two birds of prey are the #{birds_of_prey.join(' and ')}."
+end
+
+# Passing Parameters Part 3
+# 1
+gather(items) do |*items , wheat|
+  puts "#{items.join(', ')}"
+  puts "#{wheat}"
+end
+# 2
+gather(items) do |apples , *items , wheat |
+  puts "#{apples}"
+  puts "#{items.join(', ')}"
+  puts "#{wheat}"
+end
+# 3
+gather(items) do |apples , *items|
+  puts "#{apples}"
+  puts "#{items.join(', ')}"
+end
+# 4
+gather(items) do | apples, corn, cabbage, wheat|
+  puts "#{apples}, #{corn}, #{cabbage}, #{wheat}"
+end
+
+# Exploring Procs, Lambdas, and Blocks: Definition and Arity
+# Procs/Blocks do not enforce the number of arguments, Lamdas do.
+# Lamdas are a type of Proc.
+# If a yield does not pass over enough arguments to initalize all block variables, the remaining
+# ones are nil.
+# If there is a yield without a block, an error is thrown.
+
+# Exploring Procs, Lambdas, and Blocks: Returning
+# Procs/Blocks are strange with return.  The return will kick out of a method if the block is defined within a method.
+# the return will be an error if the Proc/Block is defined outside of a method.
+# Lambdas act as their own scope, it seems, as return does not effect the rest of methods.
--- Exercises/test.rb
+++ Exercises/test.rb
@@ -1,42 +1,52 @@
-require 'pry'
+# # Group 1
+# def check_return_with_proc
+#   my_proc = proc { return }
+#   my_proc.call
+#   puts "This will never output to screen."
+# end
 
+# check_return_with_proc
 
-def each_cons(array, desired_elements)
-  array.each_with_index do |item, index|
-    break if index + desired_elements > array.size
-    yield(item, array[index..(index + desired_elements-1)])
-    #binding.pry
-  end
+#The return was called and execution left the method.
+
+# Group 2
+# my_proc = proc { return }
+
+# def check_return_with_proc_2(my_proc)
+#   my_proc.call
+# end
+
+# check_return_with_proc_2(my_proc)
+
+#This gives an error, unexpected return.  so the proc defined outside a method gives an error.
+
+# # Group 3
+# def check_return_with_lambda
+#   my_lambda = lambda { return }
+#   my_lambda.call
+#   puts "This will be output to screen."
+# end
+
+# check_return_with_lambda
+
+#Lambda defined in method does not return out of the method, outputs.
+
+# # Group 4
+# my_lambda = lambda { return }
+# def check_return_with_lambda(my_lambda)
+#   my_lambda.call
+#   puts "This will be output to screen."
+# end
+
+# check_return_with_lambda(my_lambda)
+
+#Lamba defined outside of method does not return out of method.  Like Lambad is it's own universe.
+
+# Group 5
+def block_method_3
+  yield
 end
 
+block_method_3 { return }
 
-
-# hash = {}
-# each_cons([1, 3, 6, 10], 1) do |value|
-#   hash[value] = true
-# end
-# puts hash == { 1 => true, 3 => true, 6 => true, 10 => true }
-
-hash = {}
-each_cons([1, 3, 6, 10], 2) do |value1, value2|
-  hash[value1] = value2
-end
-puts hash #== { 1 => 3, 3 => 6, 6 => 10 }
-
-# hash = {}
-# each_cons([1, 3, 6, 10], 3) do |value1, *values|
-#   hash[value1] = values
-# end
-# puts hash == { 1 => [3, 6], 3 => [6, 10] }
-
-# hash = {}
-# each_cons([1, 3, 6, 10], 4) do |value1, *values|
-#   hash[value1] = values
-# end
-# puts hash == { 1 => [3, 6, 10] }
-
-# hash = {}
-# each_cons([1, 3, 6, 10], 5) do |value1, *values|
-#   hash[value1] = values
-# end
-# puts hash == {}
+#Seing a block to the method caused an error.  This makes sense since Procs did it too.
